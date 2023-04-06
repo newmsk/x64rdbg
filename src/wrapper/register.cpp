4356e6a5dfb6b331c5c67f03793652cd45158a0d
lujiarui <1451723893@qq.com>
2023-04-07 00:21:23 +0800

    update cxx regs enum

--- src/wrapper/register.cpp
+++ src/wrapper/register.cpp
@@ -1,12 +1,36 @@
-#include "register.h"
+#include <map>
 
+#include "register.h"
 #include "pluginmain.h"
+
+
+Script::Register::RegisterEnum get_real_reg_id(::reg::RegisterEnum wrap_id)
+{
+	std::map<::reg::RegisterEnum, Script::Register::RegisterEnum> cxx_reg_id_map = {
+		{::reg::CIP, Script::Register::CIP},
+		{::reg::CSP, Script::Register::CSP},
+		{::reg::CAX, Script::Register::CAX},
+		{::reg::CBX, Script::Register::CBX},
+		{::reg::CCX, Script::Register::CCX},
+		{::reg::CDX, Script::Register::CDX},
+		{::reg::CDI, Script::Register::CDI},
+		{::reg::CSI, Script::Register::CSI},
+		{::reg::CBP, Script::Register::CBP},
+		{::reg::CFLAGS, Script::Register::CFLAGS},
+	};
+
+	if (wrap_id < 10000)
+		return static_cast<Script::Register::RegisterEnum>(wrap_id);
+
+	return cxx_reg_id_map[wrap_id];
+}
 
 
 Status Register::Get(::grpc::ServerContext* context, const ::reg::RegisterId* request, ::reg::RegisterValue* response)
 {
 	(void)context;
-	response->set_value(static_cast<uint64_t>(Script::Register::Get(static_cast<Script::Register::RegisterEnum>(request->id()))));
+	auto real_reg_id = get_real_reg_id(request->id());
+	response->set_value(static_cast<uint64_t>(Script::Register::Get(real_reg_id)));
 	return Status::OK;
 }
 
@@ -15,7 +39,8 @@ Status Register::Set(::grpc::ServerContext* context, const ::reg::RegisterIdValu
 	(void)context;
 	const ::reg::RegisterId id = request->id();
 	const ::reg::RegisterValue value = request->value();
-	response->set_boolean(Script::Register::Set(static_cast<Script::Register::RegisterEnum>(id.id()), static_cast<duint>(value.value())));
+	auto real_reg_id = get_real_reg_id(id.id());
+	response->set_boolean(Script::Register::Set(real_reg_id, static_cast<duint>(value.value())));
 	return Status::OK;
 }
 
